30c1d6d8124e9e3aaac82210b457fdf05be10ab3
Zhaarn Maheswaran <zhaarn@gmail.com>
2013-09-22 16:30:02 -0400

    added docstrings to Algorithm and Recsys

--- Algorithm.py
+++ Algorithm.py
@@ -5,6 +5,16 @@ import matplotlib.pyplot as plt
 class Algorithm:
     
     def _create_matrices(self, training_data, size, k=30):
+        """
+        Converts the training set into matrix form
+        
+        :param training_data: a dataset
+        :type training_data: a tuple of lists
+        :param size: dimensions of the matrix
+        :type size: tuple
+        :param k: the size of the common feature space for users and items
+        :type k: int
+        """
         print "Creating matrices.......\n"
         self._M = spmatrix(training_data[2], training_data[0], training_data[1], size)
         rated = [1]*len(training_data[2])
@@ -13,18 +23,27 @@ class Algorithm:
         self._I = uniform(k, size[1], 0, 0.2)
     
     def get_user_matrix(self):
+        """
+        Returns a matrix containing feature vectors for each user
+        """
         if self._U is not None:
             return self._U
         else:
             print "The user matrix has not been initialized"
 
     def get_item_matrix(self):
+        """
+        Returns a matrix containing feature vectors for each item 
+        """
         if self._I is not None:
             return self._I
         else:
             print "The item matrix has not been initialized"
 
     def get_training_matrix(self):
+        """
+        Returns a matrix containing the training set
+        """
         if self._M is not None:
             return self._M
         else:
@@ -32,6 +51,20 @@ class Algorithm:
 
     #TODO: Add cost printing, make constants for plot measurements
     def _gradient_descent(self, cost_function=None, gradient=None, alpha=.001, reg=.00001, plot_cost=False):
+        """
+        Performs gradient descent on the training matrix.
+
+        :param cost_function: a cost function
+        :type cost_function: function
+        :param gradient: the gradient of the cost_function
+        :type gradient: function
+        :param alpha: the learning rate
+        :type alpha: float
+        :param reg: the regularlization parameter
+        :type reg: float:
+        :param plot_cost: whether to plot the cost function
+        :type plot_cost: boolean
+        """
         print "Performing gradient descent.......\n"
         
         if cost_function is None:
@@ -60,6 +93,20 @@ class Algorithm:
             plt.show()
 
     def _gradient(self, reg, M=None, U=None, I=None, R=None):
+        """
+        Computes the value of the gradient
+
+        :param reg: the regularization parameter
+        :type reg: float
+        :param M: the training matrix
+        :type M: cvxopt spmatrix
+        :param U: the user matrix
+        :type U: cvxopt matrix
+        :param I: the item matrix
+        :type I: cvxopt matrix
+        :param R: a matrix containing a 1 where a value is present in the training matrix
+        :type R: cvxopt spmatrix
+        """
         if M is None:
             M = self._M
         if U is None:
@@ -69,12 +116,26 @@ class Algorithm:
         if R is None:
             R = self._R
             
-        #print "Calculating gradient.......\n"
+        print "Calculating gradient.......\n"
         grad_U = mul((U * I - M), R) * I.trans() + reg * U
         grad_I = (mul((U * I - M), R).trans() * U).trans() + reg * I
         return (grad_U, grad_I)
 
     def _cost_function(self, reg, M=None, U=None, I=None, R=None):
+        """
+        Computes the value of the cost function
+
+        :param reg: the regularization parameter
+        :type reg: float
+        :param M: the training matrix
+        :type M: cvxopt spmatrix
+        :param U: the user matrix
+        :type U: cvxopt matrix
+        :param I: the item matrix
+        :type I: cvxopt matrix
+        :param R: a matrix containing a 1 where a value is present in the training matrix
+        :type R: cvxopt spmatrix
+        """
         if M is None:
             M = self._M
         if U is None:
@@ -92,6 +153,14 @@ class Algorithm:
         return (0.5*cost)/(self._ltrain)
 
     def train(self, training_data, size):
+        """
+        Trains the dataset
+
+        :param training_data: the training set
+        :type training_data: a 3-tuple of lists
+        :param size: the dimensions of the training matrix
+        :type size: tuple
+        """
         print "Training.......\n"
 
         if training_data is None:
@@ -102,6 +171,16 @@ class Algorithm:
 
 
     def rmse(self, data, mean=None, biases=None):
+        """
+        Calculates the rmse of the model on a given data set
+        
+        :param data: the test data
+        :type data: 3-tuple of lists
+        :param mean: the mean of the training set
+        :type mean: float
+        :param biases: the biases of the training set
+        :type biases: a 2-tuple of float lists
+        """
         if data is None:
             print "Please supply valid test data"
 
@@ -110,6 +189,16 @@ class Algorithm:
         
     
     def _test(self, test_data, mean=None, biases=None ):
+        """
+        Calculates the rmse of the model on a given data set
+        
+        :param test_data: the test data
+        :type test_data: 3-tuple of lists
+        :param mean: the mean of the training set
+        :type mean: float
+        :param biases: the biases of the training set
+        :type biases: a 2-tuple of float lists
+        """
         data = zip(test_data[0], test_data[1], test_data[2])
         U = self._U
         I = self._I
--- Recsys.py
+++ Recsys.py
@@ -4,6 +4,20 @@ from cvxopt import matrix
 class Recsys:
 
     def __init__(self, user_matrix, item_matrix, mean=None, user_bias=None, item_bias=None):
+        """
+        Initializes the recommender system base on a trained model
+
+        :param user_matrix: a matrix containing vectors of feature values for each user
+        :type user_matrix: cvxopt matrix
+        :param item_matrix: a matrix containing vectors of feature values for each item
+        :type item_matrix: cvxopt matrix:
+        :param mean: the mean of the training set
+        :type mean: float
+        :param user_bias: a list containing the per user biases
+        :type user_bias: list
+        :param item_bias: a list containing the per item biases
+        :type item_bias: list
+        """
         if not (user_matrix or item_matrix):
             print "Please enter valid parameters"
         self._U = user_matrix
@@ -11,8 +25,25 @@ class Recsys:
         self._b = (user_bias, item_bias)
 
     def predict(self, user, item):
+        """
+        Predicts the rating a given user would give to an item
+
+        :param user: user index
+        :type user: int
+        :param item: item index:
+        :type item: int
+        """
         print "Prediction: %f" %  self._mu + self._b[0][user] - self._b[1][item] - (self._U[user, :] * self._I[:, item])[0]
     
     def similar(self, item1, item2):
+        """
+        Gives the similarity between two items/users
+        
+        :param item1: item/user index
+        :type user: int
+        :param item2: item/user index:
+        :type item: int
+        """
+        
         print "Similarity: %f" % (self._I[:, item1].trans() * self._I[:, item2])[0]
         
